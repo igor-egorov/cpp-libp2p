b72debea759c9fd25659c2dfc4088248eefde846
Igor Egorov <igor@soramitsu.co.jp>
2020-03-30 15:10:42 +0300

    sqlite version
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -37,6 +37,6 @@ hunter_config(
 
 hunter_config(
     SQLiteModernCpp
-    URL https://github.com/soramitsu/libp2p-sqlite-modern-cpp/archive/fc3b700064cb57ab6b598c9bc7a12b2842f78da2.zip
-    SHA1 d913f2a0360892a30bc7cd8820a0475800b47d76
+    URL https://github.com/igor-egorov/sqlite_modern_cpp/archive/efb802aaa0c9a474343931b1f93160ca37ceaedf.zip
+    SHA1 fd38198809454569ed7e495700bd6aee8ff61c6b
 )
