7dcace9a9b0e2e3ae5429d1b2d281ea620aeead1
Igor Egorov <igor@soramitsu.co.jp>
2020-03-30 16:27:27 +0300

    sqlite ver print
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -30,6 +30,7 @@ find_package(tsl_hat_trie CONFIG REQUIRED)
 hunter_add_package(Boost.DI)
 find_package(Boost.DI CONFIG REQUIRED)
 
+message(STATUS "HUNTER SQLiteModernCpp VERSION ${HUNTER_SQLiteModernCpp_VERSION}")
 # https://github.com/soramitsu/libp2p-sqlite-modern-cpp/tree/hunter
 hunter_add_package(SQLiteModernCpp)
 find_package(SQLiteModernCpp CONFIG REQUIRED)
